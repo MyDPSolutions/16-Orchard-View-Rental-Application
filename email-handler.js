e9e6ad5207379587ee678a2afb8303d09877ab4f
MyDPSolutions <38671491+MyDPSolutions@users.noreply.github.com>
2026-08-23 17:02:10 -0400

    Make rental application email submission reliable

--- email-handler.js
+++ email-handler.js
@@ -3,117 +3,43 @@
 EMAIL HANDLER
 ========================================== */
 
-
 (function(){
-
-emailjs.init({
-
-publicKey:
-
-"OlQi5J74Uve2qhqRu"
-
-});
-
-
+    emailjs.init({
+        publicKey: "OlQi5J74Uve2qhqRu"
+    });
 })();
 
-
-
-
+function buildApplicationSummary(data){
+    return Object.keys(data)
+        .filter(key => data[key] !== undefined && data[key] !== null && String(data[key]).trim() !== "")
+        .map(key => `${friendlyLabel(key)}: ${data[key]}`)
+        .join("\n");
+}
 
 async function sendApplicationEmail(){
+    const data = collectApplicationData();
+    const applicantName = `${data.firstName || ""} ${data.lastName || ""}`.trim();
 
+    const params = {
+        application_number: data.applicationNumber,
+        applicant_name: applicantName,
+        applicant_email: data.email,
+        submission_date: data.submissionDate,
+        property_address: "16 Orchard View Drive",
+        message: "New rental application received for 16 Orchard View Drive.",
+        application_summary: buildApplicationSummary(data)
+    };
 
-let data =
-collectApplicationData();
-
-
-
-let pdf =
-generatePDF();
-
-
-
-let pdfBlob =
-pdf.output("blob");
-
-
-
-let applicantName =
-data.firstName
-+
-" "
-+
-data.lastName;
-
-
-
-let params = {
-
-
-application_number:
-data.applicationNumber,
-
-
-applicant_name:
-applicantName,
-
-
-applicant_email:
-data.email,
-
-
-submission_date:
-data.submissionDate,
-
-
-message:
-
-"New rental application received for 16 Orchard View Drive."
-
-};
-
-
-
-
-
-emailjs.send(
-
-"service_iuhvgrh",
-
-"template_j94vppx",
-
-params
-
-)
-
-.then(function(){
-
-
-alert(
-
-"Your application has been submitted successfully."
-
-);
-
-
-})
-
-
-.catch(function(error){
-
-
-console.log(error);
-
-
-alert(
-
-"There was an error submitting your application. Please try again."
-
-);
-
-
-});
-
-
+    try {
+        await emailjs.send(
+            "service_iuhvgrh",
+            "template_j94vppx",
+            params
+        );
+        return true;
+    }
+    catch(error){
+        console.error("EmailJS submission error:", error);
+        throw error;
+    }
 }
